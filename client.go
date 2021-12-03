e57ee76d95158efe01fe7c1abff46e68a2f05f8c
cs <cs@gmail.com>
2021-12-03 17:46:04 +0530

    client requestin info

--- client.go
+++ client.go
@@ -16,6 +16,7 @@ func main() {
 	defer conn.Close()
 	c := mangment.NewUsermanagementClient(conn)
 	var m = map[string]int64{"Srikantha": 22, "anil": 23}
+	var id = []int32{1, 2}
 	for k, v := range m {
 		res, err := c.CreateNewUser(context.Background(), &mangment.NewUser{Name: k, Age: v})
 		if err != nil {
@@ -24,4 +25,12 @@ func main() {
 		fmt.Printf("Name:%s\n Age:%d \n Id:%s\n", res.Name, res.Age, res.Id)
 
 	}
+	for _, i := range id {
+		res1, err := c.GetaUser(context.Background(), &mangment.UserId{Id: i})
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("Id=%d\tName:%s\tAge:%d\tPhone:%d", res1.Id, res1.Name, res1.Age, res1.Phone)
+	}
+
 }
